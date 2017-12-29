2001f6b17ebd4b4d1d1ad59c18ac5c16473e5cac
slemenik <ms4388@student.uni-lj.si>
2017-12-29 20:11:46 +0100

    Made project structure, draw simple line

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// The distance between the points:
+
+var path = new Path({
+    strokeColor: '#000000',
+    strokeWidth: 10,
+    strokeCap: 'round'
+});
+
+path.add(new Point(35, 50));
+path.add(new Point(70, 50));
+
